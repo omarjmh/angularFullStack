8c48649cf1dd63378368a6b4c695c8508c695860
Omar Sial <omar.sial1@gmail.com>
2015-12-12 22:49:43 -0500

    app deplyed to heroku

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('yoAngularFullStackApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
